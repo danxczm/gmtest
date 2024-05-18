d1e2569a40d7e9e7ec6de03047ae8d43599d5423
dan <81313913+danxczm@users.noreply.github.com>
2024-05-18 15:55:52 +0300

    1380 desktop to 1280

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,jsx}'],
-  theme: {
-    screens: {
-      mobile: '320px',
-      tablet: '640px',
-      desktop: '1380px',
+    content: ['./src/**/*.{html,js,jsx}'],
+    theme: {
+        screens: {
+            mobile: '320px',
+            tablet: '640px',
+            desktop: '1280px',
+        },
+        extend: {
+            colors: {
+                blue: '#0177B8',
+                green: '#179D2E',
+                red: '#F02E2C',
+                yellow: '#F5AF23',
+            },
+        },
     },
-    extend: {
-      colors: {
-        blue: '#0177B8',
-        green: '#179D2E',
-        red: '#F02E2C',
-        yellow: '#F5AF23',
-      },
-    },
-  },
-  safelist: [
-    'border-blue',
-    'border-green',
-    'border-red',
-    'border-yellow',
-    'fill-blue',
-    'fill-green',
-    'fill-red',
-    'fill-yellow',
-  ],
-  plugins: [],
+    safelist: [
+        'border-blue',
+        'border-green',
+        'border-red',
+        'border-yellow',
+        'fill-blue',
+        'fill-green',
+        'fill-red',
+        'fill-yellow',
+    ],
+    plugins: [],
 };
